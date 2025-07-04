75cd3670d2d83696f2efbc212a26ab730cb06693
seraLeeDev <seraleedev@gmail.com>
2025-07-04 17:22:18 +0900

    feature: 전체 할일, 완료 할일 카운팅 기능 추가

--- util/index.js
+++ util/index.js
@@ -1,6 +1,7 @@
 export let todoListArray = [];
-export let completedTodo = todoListArray.filter((todo) => todo.isCompleted);
+
 const Ul = document.querySelector("#todo-list");
+const CountContainer = document.querySelector("#count-container");
 const TODOLIST = "toDoList";
 
 // 로컬스토리지 데이터 로드 함수
@@ -12,6 +13,7 @@ export const loadTodo = () => {
     todoListArray = localTodoList;
     todoListArray.forEach((item) => renderItem(item));
   }
+  renderTodoCount();
 };
 
 // 데이터 추가시 로컬스토리지 저장 함수
@@ -21,11 +23,16 @@ const saveNewTodo = (data) => {
 };
 
 // 수정 데이터 저장함수
-const saveEditList = (id, editData) => {
+const saveEditList = (id, key, value) => {
   const targetItem = todoListArray.find((item) => item.id == id);
+  const editData = {
+    ...targetItem,
+    [key]: value,
+  };
   const targetIndex = todoListArray.indexOf(targetItem);
   todoListArray[targetIndex] = editData;
   localStorage.setItem(TODOLIST, JSON.stringify(todoListArray));
+  renderTodoCount();
 };
 
 // 할일 추가 핸들러
@@ -40,6 +47,7 @@ export const submitNewData = (event) => {
   renderItem(newData);
   saveNewTodo(newData);
   addText.value = "";
+  renderTodoCount();
 };
 
 // 삭제 버튼 클릭 핸들러
@@ -48,16 +56,13 @@ export const deleteTodo = (id) => {
   localStorage.setItem(TODOLIST, JSON.stringify(todoListArray));
   Ul.innerHTML = "";
   todoListArray.forEach((item) => renderItem(item));
+  renderTodoCount();
 };
 
 // 수정 버튼 클릭 핸들러
 export const editTodo = (target, id) => {
   const newValue = target.previousSibling.value;
-  const editData = {
-    ...targetItem,
-    name: newValue,
-  };
-  saveEditList(id, editData);
+  saveEditList(id, "name", newValue);
 };
 
 // 체크박스 클릭 핸들러
@@ -75,11 +80,7 @@ const checkTodo = (target, id) => {
     targetEditBtn.disabled = false;
   }
 
-  const editData = {
-    ...targetItem,
-    isCompleted: target.checked,
-  };
-  saveEditList(id, editData);
+  saveEditList(id, "isCompleted", target.checked);
 };
 
 // 리스트 노드 생성
@@ -111,4 +112,10 @@ export const renderItem = (data) => {
   Li.appendChild(EditBtn);
   Li.appendChild(DeleteBtn);
   Ul.appendChild(Li);
+};
+
+// 카운트 노드 생성
+export const renderTodoCount = () => {
+  const completedTodo = todoListArray.filter((todo) => todo.isCompleted);
+  CountContainer.innerHTML = `<p>전체: ${todoListArray.length}</p><p>완료: ${completedTodo.length}</p>`;
 };
