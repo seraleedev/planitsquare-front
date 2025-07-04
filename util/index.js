c5dfea47692fe8802d24f2d53c970fe92e5f6c12
seraLeeDev <seraleedev@gmail.com>
2025-07-04 16:35:07 +0900

    feature: 할일 제목 수정기능 추가

--- util/index.js
+++ util/index.js
@@ -1,4 +1,5 @@
 export let todoListArray = [];
+export let completedTodo = todoListArray.filter((todo) => todo.isCompleted);
 const ul = document.querySelector("#todo-list");
 const TODOLIST = "toDoList";
 
@@ -21,7 +22,7 @@ export const submitData = (event) => {
   const addText = document.querySelector("#addText");
   event.preventDefault();
   const newData = {
-    id: addText.value,
+    id: addText.value + Math.floor(Math.random() * 100).toString(),
     name: addText.value,
     isCompleted: false,
   };
@@ -37,16 +38,33 @@ export const deleteTodo = (id) => {
   todoListArray.forEach((item) => renderItem(item));
 };
 
+export const editTodo = (target, id) => {
+  const newValue = target.previousSibling.value;
+  const targetItem = todoListArray.find((item) => item.id == id);
+  const targetIndex = todoListArray.indexOf(targetItem);
+  const editData = {
+    ...targetItem,
+    name: newValue,
+  };
+  todoListArray[targetIndex] = editData;
+  localStorage.setItem(TODOLIST, JSON.stringify(todoListArray));
+};
+
 export const renderItem = (data) => {
   const li = document.createElement("li");
   const input = document.createElement("input");
   input.type = "text";
   input.value = data.name;
   input.readOnly = data.isCompleted;
+
   const deleteBtn = document.createElement("button");
+  const editBtn = document.createElement("button");
+  editBtn.textContent = "수정";
   deleteBtn.textContent = "X";
+  editBtn.addEventListener("click", (event) => editTodo(event.target, data.id));
   deleteBtn.addEventListener("click", () => deleteTodo(data.id));
   li.appendChild(input);
+  li.appendChild(editBtn);
   li.appendChild(deleteBtn);
   li.id = todoListArray.length + 1;
   ul.appendChild(li);
